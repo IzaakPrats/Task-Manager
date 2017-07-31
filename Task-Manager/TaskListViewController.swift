a7a6caeea6ec49cf66894fa93cc0fe8999e7b049
ryanbrashear <ryan.brashear@interapthq.com>
2017-07-30 21:32:52 -0400

    created the TaskListViewController and set up the task list view to use it

---
+++ Task-Manager/TaskListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskListViewController.swift
+//  Task-Manager
+//
+//  Created by Ryan Brashear on 7/30/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import UIKit
+
+class TaskListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
