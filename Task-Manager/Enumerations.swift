290d7a515a051d761e2235f14f2f925daee8cf90
ryanbrashear <ryan.brashear@interapthq.com>
2017-07-30 20:29:18 -0400

    adding the Task and Enumerations files that were also used in the command line Task Manager

---
+++ Task-Manager/Enumerations.swift
@@ -0,0 +1,15 @@
+//
+//  Enumerations.swift
+//  To-Do
+//
+//  Created by Ryan Brashear on 7/17/17.
+//  Copyright © 2017 Ryan Brashear. All rights reserved.
+//
+
+import Foundation
+
+enum Priority: String {
+    case low = "Low"
+    case medium = "Medium"
+    case high = "High"
+}
